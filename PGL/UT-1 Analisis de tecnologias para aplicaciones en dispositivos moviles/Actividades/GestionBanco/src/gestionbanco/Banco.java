502befbc6674f91d393901e3aecc87f2845e3cbb
Javier Martin <javiermartin.gara@gmail.com>
2021-09-21 18:17:53 +0100

    Creacion directorio PGL y proyecto de Gestion de Banco

---
+++ PGL/UT-1 Analisis de tecnologias para aplicaciones en dispositivos moviles/Actividades/GestionBanco/src/gestionbanco/Banco.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionbanco;
+
+import java.util.List;
+
+/**
+ *
+ * @author Javier Martin Lorenzo <javiermartin.gara@gmail.com>
+ */
+public class Banco {
+    
+    private String direccion;
+    private List<String> monedas;
+            
+    
+}
---
+++ PGL/UT-1 Analisis de tecnologias para aplicaciones en dispositivos moviles/Actividades/GestionBanco/src/gestionbanco/Cuenta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionbanco;
+
+/**
+ *
+ * @author Javier Martin Lorenzo <javiermartin.gara@gmail.com>
+ */
+public class Cuenta {
+    
+    private int numero;
+    private double saldo;
+    
+    
+}
