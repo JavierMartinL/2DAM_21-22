dcde978ba605759e02381fb72e9b6d36d4077144
Javier Martin <javiermartin.gara@gmail.com>
2021-09-27 01:41:52 +0100

    Editor de texto (textArea)

---
+++ AD/UT-1 Manejo de ficheros/Actividades/editorTextoTextArea/src/es/iespuertodelacruz/javier/editortextotextarea/EditorTextoTextArea.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.iespuertodelacruz.javier.editortextotextarea;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Javier Martin Lorenzo <javiermartin.gara@gmail.com>
+ */
+public class EditorTextoTextArea extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/es/iespuertodelacruz/javier/editortextotextarea/view/FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Editor texto (textArea)");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
