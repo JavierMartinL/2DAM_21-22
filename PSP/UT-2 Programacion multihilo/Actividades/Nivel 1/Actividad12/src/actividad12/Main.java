c6fa11264c8f014d5faae6aa5870da83be8c47e6
Javier Martin <javiermartin.gara@gmail.com>
2021-10-13 16:27:49 +0100

    Actividad12 Comenzada en clase

---
+++ PSP/UT-2 Programacion multihilo/Actividades/Nivel 1/Actividad12/src/actividad12/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad12;
+
+/**
+ *
+ * @author Javier Martin Lorenzo <javiermartin.gara@gmail.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Saludar saludar = new Saludar();
+        
+        Alumno javier = new Alumno("javier", saludar, true);
+        Alumno juan = new Alumno("juan", saludar, false);
+        Alumno adrian = new Alumno("adrian", saludar, false);
+        Alumno luis = new Alumno("luis", saludar, false);
+        
+        juan.start();
+        adrian.start();
+        luis.start();
+        javier.start();
+        
+        try{
+            juan.join();
+            adrian.join();
+            luis.join();
+            javier.join();
+        }catch(InterruptedException e ){ }
+        
+    }
+    
+}
