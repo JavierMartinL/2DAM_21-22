afbcc0471f2033dfa0323c9c1c7469f55cedc080
Javier Martin <javiermartin.gara@gmail.com>
2021-10-29 17:53:08 +0100

    Primer proyecto con DDBB

--- AD/UT-2 Bases de datos_Manejo de conectores/Prueba/src/segundadb/Main.java
+++ AD/UT-2 Bases de datos_Manejo de conectores/Prueba/src/segundadb/Main.java
@@ -3,22 +3,24 @@ package segundadb;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import segundadb.utils.GestorBaseDatos;
+import segundadb.utils.models.GestorLapices;
+import segundadb.utils.models.Lapiz;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		GestorBaseDatos gbd = new GestorBaseDatos("oficina", "root", "1q2w3e4r");
 		
-		Connection conexion = gbd.getConnection();
-		try {
-			Statement st = conexion.createStatement();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		GestorLapices gl = new GestorLapices(gbd);
+		
+		
+		ArrayList<Lapiz> lapices = (ArrayList<Lapiz>) gl.leerTodo();
+		
+		lapices.forEach(System.out::println);
 		
 	}
 }
---
+++ AD/UT-2 Bases de datos_Manejo de conectores/Prueba/src/segundadb/utils/models/Lapiz.java
@@ -0,0 +1,46 @@
+package segundadb.utils.models;
+
+public class Lapiz {
+	
+	private int idLapiz;
+	private String marca;
+	private int numero;
+	
+	public Lapiz() { }
+	
+	public Lapiz(int idLapiz, String marca, int numero) {
+		this.idLapiz = idLapiz;
+		this.marca = marca;
+		this.numero = numero;
+	}
+
+	public int getIdLapiz() {
+		return idLapiz;
+	}
+
+	public void setIdLapiz(int idLapiz) {
+		this.idLapiz = idLapiz;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	@Override
+	public String toString() {
+		return "Lapiz [idLapiz=" + idLapiz + ", marca=" + marca + ", numero=" + numero + "]";
+	}
+	
+}
