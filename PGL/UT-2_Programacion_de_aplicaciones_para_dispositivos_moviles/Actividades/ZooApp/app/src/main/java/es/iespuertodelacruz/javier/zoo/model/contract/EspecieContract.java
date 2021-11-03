1f0b66e7619c59bbfa16e7a2de1c79503d246fa8
Javier Martin <javiermartin.gara@gmail.com>
2021-11-03 16:14:11 +0000

    Ajuste en el Contract de Especie

--- PGL/UT-2_Programacion_de_aplicaciones_para_dispositivos_moviles/Actividades/ZooApp/app/src/main/java/es/iespuertodelacruz/javier/zoo/model/contract/EspecieContract.java
+++ PGL/UT-2_Programacion_de_aplicaciones_para_dispositivos_moviles/Actividades/ZooApp/app/src/main/java/es/iespuertodelacruz/javier/zoo/model/contract/EspecieContract.java
@@ -7,7 +7,7 @@ public class EspecieContract {
     public static abstract class EspecieEntry implements BaseColumns {
         public static final String TABLE_NAME = "especie";
 
-            public static final String NOMBRE_VULGAR = "nombreVulgar";
+        public static final String NOMBRE_VULGAR = "nombreVulgar";
         public static final String NOMBRE_CIENTIFICO = "nombreCientifico";
         public static final String FAMILIA = "familia";
         public static final String PELIGRO_EXTINCION = "peligroExtincion";
