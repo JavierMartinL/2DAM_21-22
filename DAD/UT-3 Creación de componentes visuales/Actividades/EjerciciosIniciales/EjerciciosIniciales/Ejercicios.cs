093c82a95bc9a2874cd1542576fef4f4facba2a2
Javier Martin <javiermartin.gara@gmail.com>
2021-11-04 01:51:44 +0000

    DAD: Ejercicios iniciales 1, 2, 3, 4

---
+++ DAD/UT-3 Creación de componentes visuales/Actividades/EjerciciosIniciales/EjerciciosIniciales/Ejercicios.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosIniciales
+{
+    class Ejercicios
+    {
+
+        /**
+         * Realiza una aplicación que pregunte un número por teclado y devuelva una 'piramide vertical' con 
+         * altura el número que nos han facilitado.
+         */
+        public static void ejercicio1(int altura)
+        {
+            // Aumentar *
+            for (int i = 1; i <= altura; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            // Disminuir *
+            for (int i = altura - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /**
+         * Realizar una función que dado un DNI (comprobar que es un número entre 0 y 99999999), devuelva la 
+         * letra asociado al mismo. Esta letra se calcula como el módulo (resto) de la división del número del 
+         * DNI entre 23, y a partir del resto se escoge una de las siguientes letras según la posición: TRWAGMYFPDXBNJZSQVHLCKET.
+         * Por ejemplo, para el DNI 12345678, al dividir entre 23 se obtiene como resto 14, por lo que le 
+         * corresponde la letra 'Z', siendo el DNI correcto 12345678Z.
+         */
+        public static void ejercicio2(int numDni)
+        {
+            String cadenaLetras = "TRWAGMYFPDXBNJZSQVHLCKET";
+
+            if (numDni >= 0 && numDni <= 99999999)
+            {
+                int posLetra = numDni % 23;
+                char letra = cadenaLetras.ToCharArray()[posLetra];
+
+                Console.WriteLine("La letra para el DNI es {0} -> {1}-{0}", letra, numDni);
+            }
+            else
+            {
+                Console.WriteLine("Este número no es valido para un DNI");
+            }
+
+        }
+
+        /**
+         * Programa que nos dice qué índice de masa corporal tenemos a partir de nuestro peso en kilogramos y 
+         * nuestra altura en metros.
+         */
+        public static void ejercicio3(double peso, double altura)
+        {
+            double imc = peso / (altura * altura);
+
+            Console.WriteLine("Tu IMC es de : {0}", imc);
+        }
+
+        /**
+         * Pide al usuario 2 valores numéricos, e indica cuál es más grande de los dos. Si son iguales indicarlo también.
+         */
+        public static void ejercicio4(int num1, int num2)
+        {
+            String mensaje = "";
+            if (num1 > num2)
+            {
+                mensaje = "El número " + num1 + " es el mayor";
+            } else if (num1 < num2)
+            {
+                mensaje = "El número " + num2 + " es el mayor";
+            } else
+            {
+                mensaje = "Ambos números son iguales";
+            }
+
+            Console.WriteLine(mensaje);
+        }
+
+        /**
+         * Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también debemos indicarlo. 
+         * En caso afirmativo que calcule el factorial (n * (n-1) * ... * 1)
+         */
+        public void ejercicio5()
+        {
+
+        }
+
+        /**
+         * Crear una aplicación que nos permite insertar números hasta que insertamos un -1. Calcular el total de números introducidos, 
+         * cuál es el más pequeño, cuál es el más grande, y el más repetido.
+         */
+        public void ejercicio6()
+        {
+
+        }
+
+        /**
+         * Desarrollar un programa para jugar a "Adivina el número". Se parte de un número entero positivo previamente determinado 
+         * (por ejemplo 15). Se pide al usuario que introduzca un valor numérico. Si el dato introducido por el usuario coincide con el almacenado,
+         * el juego termina. Si el dato introducido por el usuario es menor (respectivamente mayor) que el almacenado, se indica esta 
+         * situación al usuario y se le vuelve a pedir que introduzca otro número. El juego termina cuando el usuario acierta o si llega 
+         * a las 5 oportunidades.
+         */
+        public void ejercicio7()
+        {
+
+        }
+
+        /**
+         * Aplicación de calculadora que muestre un menú con las opciones sumar, restar, multiplicar, dividir y salir. A continuación 
+         * pida dos números, y por último la operación. Y devuelva el resultado.
+         */
+        public void ejercicio8()
+        {
+
+        }
+
+        /**
+         * Crear un array de 20 números aleatorios enteros aleatorios entre 10 y 20. Para cada elemento, informar si es la primera vez 
+         * que aparece, o si se trata de un elemento repetido y nos indique cuántas veces había aparecido con anterioridad. 
+         * Con Diccionary: https://docs.microsoft.com/es-es/dotnet/api/system.collections.generic.dictionary-2?view=net-5.0
+         */
+        public void ejercicio9()
+        {
+
+        }
+
+        /**
+         * Añadir el tratamiento de errores a los ejercicios anteriores
+         * En los ejercicios:
+         * > 5 - bucles hacer el factorial con recursividad.
+         * > 6.- bucles utilizar arrayList
+         * > 8.- iterativas, funciones(calculadora) utilizar una clase propia que recoja los operandos y que como métodos realice las operaciones
+         */
+        public void ejercicio10()
+        {
+
+        }
+
+        /**
+         *  Crea un programa que nos dejará realizar diferentes acciones a escoger en menú siguiente:
+         *  •  Mostrar el archivo 
+         *  •  Añadir un texto al final del archivo 
+         *  •  Borrar una palabra y nos devolverá el texto de nuevo pero sin la palabra escogida 
+         *  •  Guardar el archivo con el mismo nombre 
+         *  •  Salir 
+         */
+        public void ejercicio11()
+        {
+
+        }
+
+    }
+}
---
+++ DAD/UT-3 Creación de componentes visuales/Actividades/EjerciciosIniciales/EjerciciosIniciales/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace EjerciciosIniciales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool finalizar = false;
+            do
+            {
+                Program.menu();
+                short opt = -1;
+
+                try
+                {
+                    opt = short.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n\tERROR: El valor introducido no es válido\n");
+                }
+
+                Console.WriteLine();
+
+                switch (opt)
+                {
+                    case 0:
+                        finalizar = true;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Ejercicio 1");
+
+                        try
+                        {
+                            Console.Write("Introduce la altura de la piramide: ");
+                            int altura = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("La altura introducida es: {0} \n", altura);
+
+                            Ejercicios.ejercicio1(altura);
+                        } catch (Exception ex)
+                        {
+                            Console.WriteLine("\n\tERROR: Debes introducir un número\n");
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Ejercicio 2");
+
+                        try
+                        {
+                            Console.Write("Introduce el numero del DNI para asignar una letra: ");
+                            int numDni = Int32.Parse(Console.ReadLine());
+
+                            Ejercicios.ejercicio2(numDni);
+                        } catch (Exception ex)
+                        {
+                            Console.WriteLine("\n\tERROR: Debes introducir un número\n");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Ejercicio 3");
+
+                        try
+                        {
+                            Console.Write("Introduce tu peso (kg): ");
+                            double peso = double.Parse(Console.ReadLine());
+                            Console.Write("Introduce tu altura (m): ");
+                            double altura = double.Parse(Console.ReadLine());
+
+                            Ejercicios.ejercicio3(peso, altura);
+                        } catch (Exception ex)
+                        {
+                            Console.WriteLine("\n\tERROR: Debes introducir un número\n");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Ejercicio 4");
+
+                        Console.WriteLine("Calcular que número es mayor");
+                        try
+                        {
+                            Console.Write("Introduce el primer número: ");
+                            int num1 = Int32.Parse(Console.ReadLine());
+                            Console.Write("Introduce el segundo número: ");
+                            int num2 = Int32.Parse(Console.ReadLine());
+
+                            Ejercicios.ejercicio4(num1, num2);
+                        } catch (Exception ex)
+                        {
+                            Console.WriteLine("\n\tERROR: Debes introducir un número\n");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finalizar);
+
+        }
+
+        private static void menu()
+        {
+            String ejercicios = "\t1)  Ejercicio 1" +
+                "\n\t2)  Ejercicio 2" +
+                "\n\t3)  Ejercicio 3" +
+                "\n\t4)  Ejercicio 4" +
+                "\n\t5)  Ejercicio 5" +
+                "\n\t6)  Ejercicio 6" +
+                "\n\t7)  Ejercicio 7" +
+                "\n\t8)  Ejercicio 8" +
+                "\n\t9)  Ejercicio 9" +
+                "\n\t10) Ejercicio 10" +
+                "\n\t11) Ejercicio 11" +
+                "\n\t0)  Salir";
+
+            Console.WriteLine("\nMenu de Ejercicios:");
+            Console.WriteLine(ejercicios);
+            Console.Write("\nQue ejercicio quieres ejecutar: ");
+
+        }
+    }
+}
