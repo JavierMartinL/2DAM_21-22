4138b96aef3d7c7a1bc69dbdb1ca553ea6ba730b
Javier Martin <javiermartin.gara@gmail.com>
2021-11-04 19:01:44 +0000

    DAD: Creacion del proyecto AEUT3_02_Ejercicios_CS

---
+++ DAD/UT-3 Creación de componentes visuales/Actividades/AEUT3_02_Ejercicios_CS/AEUT3_02_Ejercicios_CS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AEUT3_02_Ejercicios_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
