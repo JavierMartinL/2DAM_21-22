21afbc3f8cd1b6a52c1d9bff99a864461dc2e826
Javier Martin <javiermartin.gara@gmail.com>
2021-11-16 16:49:28 +0000

    DAD: Ajuste Controlador

---
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_03_ClinicaA_WF/Controller/RegistroController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View;
+
+namespace Controller
+{
+    class RegistroController
+    {
+        RegistroView view;
+        public RegistroController()
+        {
+            this.view = new RegistroView();
+            view.Show();
+        }
+
+        private void login(object sender, EventArgs e)
+        {
+            Console.WriteLine(view.txbUser.Text);
+        }
+    }
+}
