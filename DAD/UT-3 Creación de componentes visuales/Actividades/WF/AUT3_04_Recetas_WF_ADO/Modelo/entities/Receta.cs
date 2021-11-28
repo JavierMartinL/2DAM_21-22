978060399e58ab79676be4f4a7b36da28d5fd21a
Javier Martin <javiermartin.gara@gmail.com>
2021-11-28 03:52:55 +0000

    DAD: Proyecto Receta entity Receta

--- DAD/UT-3 Creación de componentes visuales/Actividades/WF/AUT3_04_Recetas_WF_ADO/Modelo/entities/Receta.cs
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AUT3_04_Recetas_WF_ADO/Modelo/entities/Receta.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Modelo.entities
 {
-    class Receta
+    public class Receta
     {
+        private int id;
+        private string nombre;
+        private string categoria;
+        private string tiempo;
+        private string ingredientes;
+        private string descripcion;
+
+        public Receta() { }
+
+        public Receta(int id, string nombre, string categoria, string tiempo, string ingredientes, string descripcion)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.categoria = categoria;
+            this.tiempo = tiempo;
+            this.ingredientes = ingredientes;
+            this.descripcion = descripcion;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+        public string Tiempo { get => tiempo; set => tiempo = value; }
+        public string Ingredientes { get => ingredientes; set => ingredientes = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
     }
 }
