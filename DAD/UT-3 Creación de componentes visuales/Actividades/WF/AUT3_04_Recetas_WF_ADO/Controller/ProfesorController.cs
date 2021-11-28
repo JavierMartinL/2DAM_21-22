9671412a084786fa110eb370513c9b0e1d60b993
Javier Martin <javiermartin.gara@gmail.com>
2021-11-28 21:15:18 +0000

    DAD: Proyecto Receta Comentarios en Controller

--- DAD/UT-3 Creación de componentes visuales/Actividades/WF/AUT3_04_Recetas_WF_ADO/Controller/ProfesorController.cs
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AUT3_04_Recetas_WF_ADO/Controller/ProfesorController.cs
@@ -19,26 +19,42 @@ namespace Controller
             recetaDao = new RecetaDAO();
         }
 
+        /*
+         * Método que recoge las categorías que existen en la base de datos y devuelve la lista de categorías existentes
+         */
         public List<string> recogerCategorias()
         {
             return recetaDao.findCategorias();
         }
 
+        /*
+         * Método que busca pide las recetas según la categoría y devuelve un DataTable con los datos de la recetas
+         */
         public DataTable buscarRecetasCategoria(string categoria)
         {
+            // Pedir todas las recetas de la base de datos
             if (categoria.Equals("todas"))
                 return recetaDao.findAll();
+            // Pedir todas las recetas segun la categoria seleccionada
             else
                 return recetaDao.findByCategria(categoria);
         }
 
+        /*
+         * Método que crea el objeto de Receta para poder guardarlo en la base de datos y devuelve un true si ha salido todo bien
+         */
         public bool crearReceta(string nombre, string categoria, string tiempo, string ingredientes, string descripcion)
         {
+            // Crear la receta
             Receta receta = new Receta(nombre, categoria, tiempo, ingredientes, descripcion);
 
+            // guardarla en la base de datos
             return recetaDao.save(receta);
         }
 
+        /*
+         * Método para poder borrar una receta de la base de datos y devuelve un true si se ha borrado correctamente
+         */
         public bool eliminarReceta(int value)
         {
             return recetaDao.delete(value);
--- DAD/UT-3 Creación de componentes visuales/Actividades/WF/AUT3_04_Recetas_WF_ADO/Controller/RegistroController.cs
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AUT3_04_Recetas_WF_ADO/Controller/RegistroController.cs
@@ -18,13 +18,18 @@ namespace Controller
             usuarioDAO = new UsuarioDAO();
         }
 
+        /*
+         * Método que comprueba que la contraseña introducida es igual a la del usuario y devuelve el rol del usuario
+         */
         public string login(string user, string password)
         {
             string rol = "";
+            // Recoger los datos del Usuario
             Usuario usuario = usuarioDAO.findByName(user);
 
             if (usuario != null)
             {
+                // Comprobar que la contraseña es igual a la que esta guardada en la base de datos
                 if (usuario.Password.Equals(password))
                 {
                     rol = usuario.Rol;
