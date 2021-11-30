245ad3142523fc823f24cdb134ff52a56738ce0e
Javier Martin <javiermartin.gara@gmail.com>
2021-11-30 15:40:03 +0000

    DAD: Proyecto Tienda Inicio

--- DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/View/LoginView.Designer.cs
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/View/LoginView.Designer.cs
@@ -29,32 +29,21 @@ namespace View
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(315, 409);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
-            // 
-            // Form1
+            // LoginVIew
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::View.Properties.Resources.login;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "LoginVIew";
+            this.Text = "Login";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
