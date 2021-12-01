a59bec476b6a1c2adbfea5e3ef8c7b3dd209300f
Javier Martin <javiermartin.gara@gmail.com>
2021-12-01 18:05:06 +0000

    DAD: Proyecto Tienda login funcional

--- DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/View/LoginView.Designer.cs
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/View/LoginView.Designer.cs
@@ -34,6 +34,9 @@ namespace View
             this.txbPassword = new System.Windows.Forms.TextBox();
             this.lblPassword = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.lblErrorStaff = new System.Windows.Forms.Label();
+            this.lblErrorPassword = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblStaff
@@ -59,6 +62,7 @@ namespace View
             this.txbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbPassword.Location = new System.Drawing.Point(181, 86);
             this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PasswordChar = '*';
             this.txbPassword.Size = new System.Drawing.Size(214, 29);
             this.txbPassword.TabIndex = 3;
             // 
@@ -83,12 +87,52 @@ namespace View
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.login);
             // 
+            // lblErrorStaff
+            // 
+            this.lblErrorStaff.AutoSize = true;
+            this.lblErrorStaff.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorStaff.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorStaff.Location = new System.Drawing.Point(178, 66);
+            this.lblErrorStaff.Name = "lblErrorStaff";
+            this.lblErrorStaff.Size = new System.Drawing.Size(42, 16);
+            this.lblErrorStaff.TabIndex = 5;
+            this.lblErrorStaff.Text = "Error";
+            this.lblErrorStaff.Visible = false;
+            // 
+            // lblErrorPassword
+            // 
+            this.lblErrorPassword.AutoSize = true;
+            this.lblErrorPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorPassword.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorPassword.Location = new System.Drawing.Point(178, 118);
+            this.lblErrorPassword.Name = "lblErrorPassword";
+            this.lblErrorPassword.Size = new System.Drawing.Size(170, 16);
+            this.lblErrorPassword.TabIndex = 6;
+            this.lblErrorPassword.Text = "Introduce la contraseña";
+            this.lblErrorPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblErrorPassword.Visible = false;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(178, 129);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(321, 16);
+            this.lblError.TabIndex = 9;
+            this.lblError.Text = "Las credenciales introducidas no son validas";
+            this.lblError.Visible = false;
+            // 
             // LoginVIew
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(599, 154);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblErrorPassword);
+            this.Controls.Add(this.lblErrorStaff);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txbPassword);
             this.Controls.Add(this.lblPassword);
@@ -111,6 +155,9 @@ namespace View
         private System.Windows.Forms.TextBox txbPassword;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblErrorStaff;
+        private System.Windows.Forms.Label lblErrorPassword;
+        private System.Windows.Forms.Label lblError;
     }
 }
 
--- DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/View/LoginView.cs
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/View/LoginView.cs
@@ -22,16 +22,58 @@ namespace View
             InitializeComponent();
         }
 
+        /*
+         * Método que recoger los valores del formulario, comprueba que no existan errores y los envía al controlador
+         */
         private void login(object sender, EventArgs e)
         {
+            // Restablecer los errores
+            bool error = lblErrorStaff.Visible = lblErrorPassword.Visible = lblError.Visible = false;
+
             // Recoger los datos del usuario
-            int staff = int.Parse(txbStaff.Text);
+            string strStaff = txbStaff.Text;
+            int staff = 0;
             string password = txbPassword.Text;
             string rol = null;
 
-            // enviar datos al controlador
-            rol = loginController.login(staff, password);
+            // Controlar los errores en los textBox
+            // Controlar que introduzcan un valor en el staff
+            if (strStaff.Trim().Equals("")) {
+                lblErrorStaff.Text = "Introduce el número de Staff";
+                error = lblErrorStaff.Visible = true;
+            } 
+            else
+            {
+                // Controlar que lo introducido se un numero
+                try
+                {
+                    staff = int.Parse(txbStaff.Text);
+                }
+                catch (Exception ex)
+                {
+                    lblErrorStaff.Text = "Este campo tiene que ser un número";
+                    error = lblErrorStaff.Visible = true;
+                }
+            }
+
+            // Controlar que introduzcan un valor en la contraseña
+            if (password.Trim().Equals(""))
+                error = lblErrorPassword.Visible = true;
             
+
+            // enviar datos al controlador si no hay errores
+            if (!error) { 
+                rol = loginController.login(staff, password);
+
+                if (!rol.Equals("")) // Login Correcto
+                {
+                    Console.WriteLine("Estas dentro");
+                }
+                else // Error con las credenciales
+                {
+                    lblError.Visible = true;
+                }
+            }
         }
     }
 }
