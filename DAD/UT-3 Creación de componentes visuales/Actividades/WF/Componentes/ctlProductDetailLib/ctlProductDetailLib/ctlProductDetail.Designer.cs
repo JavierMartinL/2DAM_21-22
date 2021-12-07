c8a7cda2af712685c581508ddf9e498043c38513
Javier Martin <javiermartin.gara@gmail.com>
2021-12-07 12:33:30 +0000

    DAD: Nuevo componente para el Proyecto Tienda y entrega de la actividad

---
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/Componentes/ctlProductDetailLib/ctlProductDetailLib/ctlProductDetail.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace ctlProductDetailLib
+{
+    partial class ctlProductDetail
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblIdValue = new System.Windows.Forms.Label();
+            this.lblNameValue = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCategoryValue = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.lblQuantityValue = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(8, 8);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(29, 18);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "ID:";
+            // 
+            // lblIdValue
+            // 
+            this.lblIdValue.AutoSize = true;
+            this.lblIdValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdValue.Location = new System.Drawing.Point(37, 8);
+            this.lblIdValue.Name = "lblIdValue";
+            this.lblIdValue.Size = new System.Drawing.Size(42, 18);
+            this.lblIdValue.TabIndex = 1;
+            this.lblIdValue.Text = "value";
+            // 
+            // lblNameValue
+            // 
+            this.lblNameValue.AutoSize = true;
+            this.lblNameValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNameValue.Location = new System.Drawing.Point(64, 31);
+            this.lblNameValue.Name = "lblNameValue";
+            this.lblNameValue.Size = new System.Drawing.Size(42, 18);
+            this.lblNameValue.TabIndex = 3;
+            this.lblNameValue.Text = "value";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(8, 31);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(57, 18);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            // 
+            // lblCategoryValue
+            // 
+            this.lblCategoryValue.AutoSize = true;
+            this.lblCategoryValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoryValue.Location = new System.Drawing.Point(89, 54);
+            this.lblCategoryValue.Name = "lblCategoryValue";
+            this.lblCategoryValue.Size = new System.Drawing.Size(42, 18);
+            this.lblCategoryValue.TabIndex = 5;
+            this.lblCategoryValue.Text = "value";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategory.Location = new System.Drawing.Point(8, 54);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(81, 18);
+            this.lblCategory.TabIndex = 4;
+            this.lblCategory.Text = "Category:";
+            // 
+            // lblQuantityValue
+            // 
+            this.lblQuantityValue.AutoSize = true;
+            this.lblQuantityValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantityValue.Location = new System.Drawing.Point(83, 77);
+            this.lblQuantityValue.Name = "lblQuantityValue";
+            this.lblQuantityValue.Size = new System.Drawing.Size(42, 18);
+            this.lblQuantityValue.TabIndex = 7;
+            this.lblQuantityValue.Text = "value";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(8, 77);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(75, 18);
+            this.lblQuantity.TabIndex = 6;
+            this.lblQuantity.Text = "Quantity:";
+            // 
+            // ctlProductDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblQuantityValue);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblCategoryValue);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblNameValue);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblIdValue);
+            this.Controls.Add(this.lblId);
+            this.Name = "ctlProductDetail";
+            this.Size = new System.Drawing.Size(188, 108);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblIdValue;
+        private System.Windows.Forms.Label lblNameValue;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCategoryValue;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label lblQuantityValue;
+        private System.Windows.Forms.Label lblQuantity;
+    }
+}
---
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/Componentes/ctlProductDetailLib/ctlProductDetailLib/ctlProductDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ctlProductDetailLib
+{
+    public partial class ctlProductDetail : UserControl
+    {
+        private string id;
+        private string nombre;
+        private string categoria;
+        private string cantidad;
+
+        public ctlProductDetail()
+        {
+            InitializeComponent();
+        }
+
+        public string Id { get => lblIdValue.Text; set => lblIdValue.Text = value; }
+
+        public string Nombre { get => lblNameValue.Text; set => lblNameValue.Text = value; }
+
+        public string Categoria { get => lblCategoryValue.Text; set => lblCategoryValue.Text = value; }
+
+        public string Cantidad { get => lblQuantityValue.Text; set => lblQuantityValue.Text = value; }
+    }
+}
