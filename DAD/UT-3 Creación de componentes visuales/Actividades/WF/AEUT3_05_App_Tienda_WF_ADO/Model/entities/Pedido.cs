a16fca434017286afe79b98ea59fe3916fd1b7fa
Javier Martin <javiermartin.gara@gmail.com>
2021-12-07 01:27:09 +0000

    DAD: Proyecto Tienda creacion de las entities Producto y Pedido

---
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/Model/entities/Pedido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.entities
+{
+    public class Pedido
+    {
+        private int id;
+        private string nombre;
+        private double total;
+
+        public Pedido() { }
+
+        public Pedido(int id, string nombre, double total)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.total = total;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public double Total { get => total; set => total = value; }
+    }
+}
--- DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/Model/entities/Producto.cs
+++ DAD/UT-3 Creación de componentes visuales/Actividades/WF/AEUT3_05_App_Tienda_WF_ADO/Model/entities/Producto.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Model.entities
 {
-    class Producto
+    public class Producto
     {
+        private int id;
+        private string nombre;
+        private string categoria;
+        private string descripcion;
+        private double precio;
+        private int cantidad;
+
+        public Producto() { }
+
+        public Producto(int id, string nombre, string categoria, string descripcion, double precio, int cantidad) {
+            this.id = id;
+            this.nombre = nombre;
+            this.categoria = categoria;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.cantidad = cantidad;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public double Precio { get => precio; set => precio = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
     }
 }
