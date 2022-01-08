3f22b1ed9dd1d96db90839372cd6be59fbc3c6de
Javier Martin <javiermartin.gara@gmail.com>
2022-01-08 01:08:19 +0000

    DAD: Creción del proyecto para la Tarea AEUT2_02_Recetas

---
+++ DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/AEUT2_02_Recetas/src/main/java/es/iespuertodelacruz/javier/recetas/controller/PrincipalController.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.javier.recetas.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class PrincipalController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
---
+++ DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/DDBB/resetas.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS `reseta`;
+CREATE DATABASE `reseta`;
+USE `reseta`;
+
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `rol` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `users` (`user`, `password`, `rol`) VALUES
+    ('admin', 'admin', 'profesor'),
+    ('juan', 'j4', 'alumno');
+
+CREATE TABLE `recetas` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nombre` varchar(255) NOT NULL,
+    `user` int(11) NOT NULL,
+    `tipo` varchar(255) NOT NULL,
+    `ingredientes` varchar(255) NOT NULL,
+    `pasos` varchar(255) NOT NULL,
+    `comensales` int NOT NULL,
+    `tiempo` int NOT NULL,
+    `calorias` int NOT NULL,
+    `imagen` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_recetas_users` FOREIGN KEY (`user`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ INSERT INTO `recetas` (`nombre`, `user`, `tipo`, `ingredientes`, `pasos`, `comensales`, `tiempo`, `calorias`, `imagen`) VALUES
+     ("Flan", 2, "Postres","-Huevos -Leche", "-Batir -Hornear", 4, 30, 140, "flan.jpg"),
+     ("Pollo", 2, "Segundo", "-Pollo -Aceite", "-Poner en la plancha", 2, 15, 200, "pollo.jpg");
