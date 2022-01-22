dfb1b3b8b029136d9ffde351218803ef3b982e1f
Javier Martin <javiermartin.gara@gmail.com>
2022-01-22 22:57:29 +0000

    DAD: AEUT_03_Calculadora -> logica y vista de la calculadora

---
+++ DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/AEUT2_03_Calculadora/AEUT2_03_Calculadora/Model/Calculadora.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AEUT2_03_Calculadora.Model
+{
+    public class Calculadora
+    {
+
+        private double resultado;
+        private double numGuardado;
+        private double numIntroducido;
+        private string operador;
+        private List<string> operaciones;
+
+        /// <summary>
+        /// Contructor por defecto que inicializa las variables necesarias
+        /// </summary>
+        public Calculadora()
+        {
+            this.resultado = this.numGuardado = this.numIntroducido = 0;
+            this.operador = "";
+            this.operaciones = new List<string>();
+        }
+
+        /// <summary>
+        /// Metodo para insercion del numero que se ha introducido el usuario
+        /// </summary>
+        /// <param name="strNumero">string con el numero que queremos almacenar</param>
+        public void introducirNumero(string strNumero)
+        {
+            numIntroducido = Double.Parse(strNumero);
+        }
+
+        /// <summary>
+        /// Metodo que realiza el calculo de la operacion insertada por el usuario
+        /// </summary>
+        /// <param name="op">operador introducido por el usuario</param>
+        public void operar(string op)
+        {
+            
+            if (operador.Equals(""))
+            {
+                resultado = numGuardado = numIntroducido;
+                numIntroducido = 0;
+            } else
+            {
+                double num = numGuardado;
+                switch (operador)
+                {
+                    case "+": // Sumar
+                        resultado = numGuardado += numIntroducido;
+                        break;
+                    case "-": // Restar
+                        resultado = numGuardado -= numIntroducido;
+                        break;
+                    case "x": // Multiplicar
+                        resultado = numGuardado *= numIntroducido;
+                        break;
+                    case "/": // Dividir
+                        resultado = numGuardado /= numIntroducido;
+                        break;
+                }
+                // Guardar la operacion realizada
+                if (!operador.Equals("="))
+                    operaciones.Add(num + " " + operador + " " + numIntroducido + " = " + resultado);
+            }
+
+            numIntroducido = 0;
+            operador = op;
+        }
+
+        /// <summary>
+        /// Metodo que limpia todos las variables utilizadas
+        /// El historial de operaciones no se limpia
+        /// </summary>
+        public void limpiar()
+        {
+            resultado = numGuardado = numIntroducido = 0;
+            operador = "";
+        }
+
+        /// <summary>
+        /// Metodo que genera un string con la operacion que estamos realizando
+        /// </summary>
+        /// <returns>string con la operacion</returns>
+        public string getOperacion()
+        {
+            if (operador.Equals("="))
+            {
+                string strOperacion = operaciones[operaciones.Count() - 1].Split('=')[0];
+                return strOperacion + "=";
+            } else
+            {
+                return numGuardado + " " + operador;
+            }
+        }
+
+        /// <summary>
+        /// Metodo que devuelve el resultado de la operacion
+        /// </summary>
+        /// <returns>string con el resultado</returns>
+        public string getResultado()
+        {
+            return "" + resultado;
+        }
+
+        /// <summary>
+        /// Metodo que devuelve todas las operaciones que han sido realizadas
+        /// </summary>
+        /// <returns>string con las operaciones</returns>
+        public string historial()
+        {
+            string historial = "";
+            foreach (string operacion in operaciones)
+            {
+                historial += operacion + "\n";
+            }
+            return historial;
+        }
+    }
+}
--- DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/AEUT2_03_Calculadora/AEUT2_03_Calculadora/View/CalculadoraView.xaml.cs
+++ DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/AEUT2_03_Calculadora/AEUT2_03_Calculadora/View/CalculadoraView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AEUT2_03_Calculadora.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +22,179 @@ namespace AEUT2_03_Calculadora.View
     /// </summary>
     public partial class CalculadoraView : Page
     {
+        private string strNumero;
+        private Calculadora calculadora;
+
+        private bool numAlmacenado;
+        private bool numDecimal;
+        private bool mostrarHistorial;
+
+        /// <summary>
+        /// Contructor que inicializa las variables necesarias para el
+        /// funcionamiento de la calculadora
+        /// </summary>
         public CalculadoraView()
         {
             InitializeComponent();
+            calculadora = new Calculadora();
+
+            // Mostrar el numero 0 al iniciar la calculadora y no mostar nada en la memoria
+            strNumero = "0";
+            Resultado.Content = strNumero;
+            Memoria.Content = " ";
+
+            // variables que controlan si creamos un numero nuevo, si es decimal
+            numAlmacenado = numDecimal = false;
+            // Variable que contrala si se muestra/oculta el Historial
+            mostrarHistorial = false;
+            controlarHistorial(null, null);
+        }
+
+        /// <summary>
+        /// Metodo que controla la accion de todos los botones
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonPulsado(object sender, RoutedEventArgs e)
+        {
+            // Recoger el boton pulsado
+            Button button = (Button)e.OriginalSource;
+
+            switch(button.Content)
+            {
+                // Si es un numero lo almacenamos y mostramos
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    string valor = button.Content.ToString();
+
+                    if (strNumero.Equals("0") || numAlmacenado)
+                    {
+                        strNumero = valor;
+                        numAlmacenado = false;
+                    }
+                    else
+                    {
+                        strNumero += valor;
+                    }
+                    break;
+
+                // Si es un operador preparamos el calculo
+                case "+":
+                case "-":
+                case "x":
+                case "/":
+                case "=":
+                    // Almacenar numero generado
+                    calculadora.introducirNumero(strNumero);
+
+                    // Preparar el calculo
+                    calculadora.operar(button.Content.ToString());
+
+                    // Mostramos los datos en la pantalla
+                    Resultado.Content = calculadora.getResultado();
+                    Memoria.Content = calculadora.getOperacion();
+
+                    // Generar un nuevo numero
+                    numAlmacenado = true;
+                    numDecimal = false;
+                    strNumero = "0";
+                    break;
+
+                // Limpiar todos los datos mostrados
+                // No se contempla el historial que se limpia al cambiar de pagina
+                case "C":
+                    calculadora.limpiar();
+                    strNumero = "0";
+                    Resultado.Content = strNumero;
+                    Memoria.Content = " ";
+                    numAlmacenado = numDecimal = false;
+                    break;
+
+                // Borrar el ultimo valor del numero introducido
+                case "⌫":
+                    if (strNumero.Length == 1)
+                        strNumero = "0";
+                    else
+                    {
+                        if (strNumero[strNumero.Length - 1] == ',')
+                            numDecimal = false;
+
+                        strNumero = strNumero.Substring(0, strNumero.Length - 1);
+                    }
+                    break;
+
+                // Invertir el numero introducido
+                case "+/-":
+                    if (!strNumero.Equals("0"))
+                    {
+                        if (strNumero.Contains("-"))
+                            strNumero = strNumero.Replace("-", "");
+                        else
+                            strNumero = "-" + strNumero;
+                    }
+                    break;
+
+                // Agregar decimales al numero
+                case ",":
+                    if (!numDecimal)
+                    {
+                        strNumero += ",";
+                        numDecimal = true;
+                        numAlmacenado = false;
+                    }
+                    break;
+            }
+
+            // Mostrar el numero que esta introduciendo el usuario
+            if (!numAlmacenado)
+            {
+                Resultado.Content = strNumero;
+            }
+
+        }
+
+        /// <summary>
+        /// Metodo que muestra/oculta el historial con los calculos que han sido realizados
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void controlarHistorial(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animacion;
+
+            if (mostrarHistorial)
+            {
+                animacion = new DoubleAnimation(300, new Duration(TimeSpan.FromSeconds(0.4)));
+                Historial.BeginAnimation(WidthProperty, animacion);
+
+                // Mostrar los calculos realizados
+                string strHistorial = calculadora.historial();
+
+                if (strHistorial.Equals(""))
+                {
+                    ContenidoHistorial.Content = "Vacio";
+                }
+                else
+                {
+                    ContenidoHistorial.Content = strHistorial;
+                }
+                
+            }
+            else
+            {
+                animacion = new DoubleAnimation(0, new Duration(TimeSpan.FromSeconds(0.2)));
+                Historial.BeginAnimation(WidthProperty, animacion);
+            }
+
+            mostrarHistorial = !mostrarHistorial;
         }
     }
 }
