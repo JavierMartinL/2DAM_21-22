a36fee1318b52f2ae95f28380f41804f67ed1aa8
Javier Martin <javiermartin.gara@gmail.com>
2022-01-23 15:31:48 +0000

    DAD: AEUT_03_Calculadora -> Estili visual de convertidor de moneda

--- DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/AEUT2_03_Calculadora/AEUT2_03_Calculadora/View/MonedaView.xaml.cs
+++ DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/AEUT2_03_Calculadora/AEUT2_03_Calculadora/View/MonedaView.xaml.cs
@@ -20,9 +20,76 @@ namespace AEUT2_03_Calculadora.View
     /// </summary>
     public partial class MonedaView : Page
     {
+        private string strNumero;
+        private bool numDecimal;
+
         public MonedaView()
         {
             InitializeComponent();
+            strNumero = "0";
+            valorIntroducido.Content = "0 $";
+            numDecimal = false;
+        }
+
+        private void buttonPulsado(object sender, RoutedEventArgs e)
+        {
+            // Recoger el boton pulsado
+            Button button = (Button)e.OriginalSource;
+
+            switch (button.Content)
+            {
+                // recoger numero pulsado
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    string valor = button.Content.ToString();
+
+                    if (strNumero.Equals("0"))
+                    {
+                        strNumero = valor;
+                    }
+                    else
+                    {
+                        strNumero += valor;
+                    }
+                    break;
+
+                // Limpiar el numero
+                case "CE":
+                    strNumero = "0";
+                    numDecimal = false;
+                    break;
+
+                // Borrar el ultimo valor del numero introducido
+                case "⌫":
+                    if (strNumero.Length == 1)
+                        strNumero = "0";
+                    else
+                    {
+                        if (strNumero[strNumero.Length - 1] == ',')
+                            numDecimal = false;
+
+                        strNumero = strNumero.Substring(0, strNumero.Length - 1);
+                    }
+                    break;
+
+                // Agregar decimales al numero
+                case ",":
+                    if (!numDecimal)
+                    {
+                        strNumero += ",";
+                        numDecimal = true;
+                    }
+                    break;
+            }
+            valorIntroducido.Content = strNumero + " $";
         }
     }
 }
