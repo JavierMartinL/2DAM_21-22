9a790d86865527e41f5d2b0a7ae1202665085b35
Javier Martin <javiermartin.gara@gmail.com>
2022-02-14 01:32:05 +0000

    DAD: AEUT5_02_Archivo_Doc_Instal_Pruebas Proyecto para desarrollar las pruebas

---
+++ DAD/UT-5 Preparacion y distribucion de aplicaciones/Actividades/ApplicationParaTest/ProyectoUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProyectoUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProyectoUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("478e41ca-e946-416b-9d18-a2680a1e5a9f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
