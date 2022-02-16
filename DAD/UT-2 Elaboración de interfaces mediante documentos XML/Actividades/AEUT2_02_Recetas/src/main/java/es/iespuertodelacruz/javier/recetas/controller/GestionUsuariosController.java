149d89289b148da55e9ec13e48ceb0ba505aa8bc
Javier Martin <javiermartin.gara@gmail.com>
2022-02-16 11:56:50 +0000

    DAD: AEUT2_02_Recetas Gestionar Usuarios

--- DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/AEUT2_02_Recetas/src/main/java/es/iespuertodelacruz/javier/recetas/controller/GestionUsuariosController.java
+++ DAD/UT-2 Elaboración de interfaces mediante documentos XML/Actividades/AEUT2_02_Recetas/src/main/java/es/iespuertodelacruz/javier/recetas/controller/GestionUsuariosController.java
@@ -5,9 +5,27 @@
  */
 package es.iespuertodelacruz.javier.recetas.controller;
 
+import es.iespuertodelacruz.javier.recetas.dao.UsuarioDAO;
+import es.iespuertodelacruz.javier.recetas.model.Usuario;
+import es.iespuertodelacruz.javier.recetas.utils.GestorDDBB;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,16 +34,111 @@ import javafx.fxml.Initializable;
  */
 public class GestionUsuariosController implements Initializable {
 
-    
-    
+    private GestorDDBB gestorDDBB;
+    private UsuarioDAO usuarioDAO;
+    private List<Usuario> usuarios;
+
+    @FXML
+    private TableView<Usuario> tbvListaUsuarios;
+    @FXML
+    private TableColumn<Usuario, Integer> clmId;
+    @FXML
+    private TableColumn<Usuario, String> clmNombre;
+    @FXML
+    private TableColumn<Usuario, String> clmPassword;
+    @FXML
+    private TableColumn<Usuario, String> clmRol;
+    @FXML
+    private Button btnCrear;
+    @FXML
+    private Button btnModificar;
+    @FXML
+    private Button btnEliminar;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        gestorDDBB = new GestorDDBB();
+        usuarioDAO = new UsuarioDAO(gestorDDBB);
+
+        clmId.setCellValueFactory(new PropertyValueFactory<Usuario, Integer>("id"));
+        clmNombre.setCellValueFactory(new PropertyValueFactory<Usuario, String>("nombre"));
+        clmPassword.setCellValueFactory(new PropertyValueFactory<Usuario, String>("password"));
+        clmPassword.setVisible(false);
+        clmRol.setCellValueFactory(new PropertyValueFactory<Usuario, String>("rol"));
+
+        cargarUsuarios();
+    }
+
+    @FXML
+    private void crearUsuario(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/CrearUsuario.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         
-    }    
-    
-    
-    
+        cargarUsuarios();
+    }
+
+    @FXML
+    private void modificarUsuario(MouseEvent event) {
+        Usuario usuario = null;
+        int index = tbvListaUsuarios.getSelectionModel().getSelectedIndex();
+        if (index >= 0) {
+            usuario = usuarios.get(index);
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/CrearUsuario.fxml"));
+                Parent root = loader.load();
+                
+                CrearUsuarioController crearUsuarioController = loader.getController();
+                crearUsuarioController.setUsuario(usuario);
+
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.setScene(scene);
+                stage.showAndWait();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            
+            cargarUsuarios();
+        }
+    }
+
+    @FXML
+    private void eliminarUsuario(MouseEvent event) {
+        Usuario usuario = null;
+        int index = tbvListaUsuarios.getSelectionModel().getSelectedIndex();
+        if (index >= 0) {
+            usuario = usuarios.get(index);
+
+            usuarioDAO.delete(usuario.getId());
+            
+            cargarUsuarios();
+        }
+    }
+
+    /**
+     * Método que recoge los usuarios de la DDBB y lo muestra en la tableview
+     */
+    private void cargarUsuarios() {
+        usuarios = usuarioDAO.findAll();
+        tbvListaUsuarios.setItems(null);
+        tbvListaUsuarios.setItems(FXCollections.observableList(usuarios));
+    }
+
 }
